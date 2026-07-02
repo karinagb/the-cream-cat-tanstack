a1054474bfe770df0547775ed122ee6513c69f1b
karinagb <karin.gb27@gmail.com>
2026-07-02 16:54:57 -0600

    trying to fix deploy 3.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './App.css'
 
-const queryClient = new QueryClient();
+
+const redirect = sessionStorage.redirect
+if (redirect) {
+  delete sessionStorage.redirect
+  window.history.replaceState(null, '', redirect)
+}
+
+const queryClient = new QueryClient()
 const router = createRouter({
   routeTree,
   basepath: import.meta.env.DEV ? '/' : '/the-cream-cat',
-});
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,4 +24,4 @@ createRoot(document.getElementById('root')).render(
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
-);
+)
