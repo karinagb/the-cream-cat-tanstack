6a983c146e95523f4a6477012f6730b5196feade
karinagb <karin.gb27@gmail.com>
2026-07-02 16:23:03 -0600

    fixing deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, basepath: '/the-cream-cat' });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
