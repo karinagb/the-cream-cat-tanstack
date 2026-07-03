1470c653eadb395397974fec86dda3b46e658ff1
karinagb <karin.gb27@gmail.com>
2026-07-02 19:12:12 -0600

    2.0

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 
 const queryClient = new QueryClient();
-const router = createRouter({routeTree,});
+const router = createRouter({
+  routeTree,
+  basepath: import.meta.env.BASE_URL,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
